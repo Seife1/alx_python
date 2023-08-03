e9ccb9aef62ca2d02e1643d754453faa6a24850d
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 12:49:55 +0300

    First commit

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """ A class which is used to compute the area of square"""
+    def __init__(self, size):
+        """ defining a funtion to instantiation 
+        
+            Args:
+                size (float): the first parameter
+            Returns: 
+                Nothing for now    
+        """
+        self.__size = size
---
+++ python-classes/README.md
@@ -0,0 +1,3 @@
+# Python OOP
+
+This is written for the task given by ALX Intro to software Engineering program on the concept Object Oriented programming with python.
