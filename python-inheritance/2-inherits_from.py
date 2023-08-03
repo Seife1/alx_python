55ecc325e1097a7f0e87dffb85d72c66e8008b0f
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 20:46:15 +0300

    PEP8

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" A module for a function that check if an object is instancec of a specified class"""
+""" A module for a function that check if an object is 
+    instancec of a specified class"""
 
 def inherits_from(obj, a_class):
     """A function which is used to check class inheritance"""
+
     return issubclass(type(obj), a_class) and not type(obj) is a_class
