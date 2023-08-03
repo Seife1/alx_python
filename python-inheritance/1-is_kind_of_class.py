6be81ef313f0b7df6e8d3093f57864d752716af0
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 20:13:02 +0300

    instance

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" A module for a function that check if an object is instancec of a specified class"""
+
+def is_kind_of_class(obj, a_class):
+    return isinstance(obj, a_class)
