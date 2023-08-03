11a0b164125a03ea879c3fc803b963282ac29534
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 20:16:04 +0300

    documented

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,4 +2,5 @@
 """ A module for a function that check if an object is instancec of a specified class"""
 
 def is_same_class(obj, a_class):
+    """A function which is used to check exact instance of a class"""
     return type(obj) is a_class
--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -2,4 +2,5 @@
 """ A module for a function that check if an object is instancec of a specified class"""
 
 def is_kind_of_class(obj, a_class):
+    """A function which is used to check instance type"""
     return isinstance(obj, a_class)
