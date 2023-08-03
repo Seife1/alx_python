591c9a8bc1da8a265b3922fbfec9f17c01659aca
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 22:38:05 +0300

    empty class

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,6 +2,14 @@
 """A module fon an empty class"""
 
 
-class BaseGeometry:
-    """An empty class"""
+class HideInitSubclassMeta(type):
+    def __dir__(cls):
+        dir_list = super().__dir__()
+        return [attr for attr in dir_list if attr != '__init_subclass__']
+    
+class MyBaseClass(metaclass=HideInitSubclassMeta):
+    def __dir__(self):
+        return [attr for attr in dir(self.__class__) if attr != '__init_subclass__']
+
+class BaseGeometry(MyBaseClass):
     pass
