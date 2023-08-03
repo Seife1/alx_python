5f01e7a5b296b03c6570a79af199570f978ffd29
Seife1 <gebrealabate19@yahoo.com>
2023-08-03 22:41:35 +0300

    Empty

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,11 +3,13 @@
 
 
 class HideInitSubclassMeta(type):
+    """Hide the __init_subclass__ for ..."""
     def __dir__(cls):
         dir_list = super().__dir__()
         return [attr for attr in dir_list if attr != '__init_subclass__']
     
 class MyBaseClass(metaclass=HideInitSubclassMeta):
+    """Hide the __init_subclass__ for ..."""
     def __dir__(self):
         return [attr for attr in dir(self.__class__) if attr != '__init_subclass__']
 
