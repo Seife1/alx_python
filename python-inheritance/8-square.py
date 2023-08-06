e9a57efe1435d8125c1e7039a7d67cd5dd1a7df9
Seife1 <gebrealabate19@yahoo.com>
2023-08-06 07:11:46 +0300

    PEP8 validation

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """A module for a class Square which is subclass of Rectangle"""
-
-
 Rectangle = __import__('7-rectangle').Rectangle
+
 
 class Square(Rectangle):
     """This is the class which inherits Rectangle class"""
@@ -12,10 +11,14 @@ class Square(Rectangle):
         self.__size = size
 
         Rectangle.integer_validator(self, "size", self.__size)
+        
     def area(self):
         """Square Area"""
+
         return self.__size ** 2
-    
+
     def __str__(self):
-        """method to specify how the object should be displayed when using the print() function."""
+        """method to specify how the object should be
+        displayed when using the print() function."""
+
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
