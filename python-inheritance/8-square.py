1ee93fa63ffc953e31b9024ee297b0057fb1d787
Seife1 <gebrealabate19@yahoo.com>
2023-08-06 07:17:53 +0300

    PEP8

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -11,6 +11,7 @@ class Square(Rectangle):
         self.__size = size
 
         Rectangle.integer_validator(self, "size", self.__size)
+
     def area(self):
         """Square Area"""
 
@@ -21,4 +22,4 @@ class Square(Rectangle):
         displayed when using the print() function."""
 
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
-    
+
