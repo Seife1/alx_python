80e8c9675e9c8b04640f6380a7273b362d6f70f1
Seife1 <gebrealabate19@yahoo.com>
2023-08-07 10:20:43 +0300

    Raise an exception

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,13 +13,13 @@ class Rectangle(Base):
 
         if type(self.__width) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
-            raise ValueError("width must be >= 0")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
 
         if type(self.__height) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
-            raise ValueError("height must be >= 0")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
 
         if type(self.__x) is not int:
             raise TypeError("x must be an integer")
