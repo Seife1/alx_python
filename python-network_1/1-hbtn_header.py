77aa1ff750f636806df05e915ca90a6679c3c827
Seife1 <gebrealabate19@yahoo.com>
2023-08-10 13:00:24 +0300

    header (sys, requests

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 """Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
 
 
-import requests, sys
+import requests
+import sys
 
 url_take = sys.argv
 if len(url_take) < 2:
