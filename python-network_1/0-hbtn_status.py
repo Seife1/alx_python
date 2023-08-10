0c1e21e0ad5346beb21c977213ba8b9461f09293
Seife1 <gebrealabate19@yahoo.com>
2023-08-10 08:45:40 +0300

    Python Network

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Project - Python Network"""
+
+
+import urllib.request
+
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+
+    try:
+        content = response.read().decode('utf-8')
+        print('Body response:')
+        print('\t- type: ', type(content))
+        print("\t- content: ", content,)
+    except Exception as e:
+        print(e)
---
+++ python-network_1/README.md
@@ -0,0 +1,3 @@
+# Python, create a network request
+
+Python offers us the urllib standard library package to create network requests.
