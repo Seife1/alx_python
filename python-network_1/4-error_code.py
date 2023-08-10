d9f3ad3fc1ff6455e99393d27b632b43bbf70361
Seife1 <gebrealabate19@yahoo.com>
2023-08-10 20:56:19 +0300

    URL error code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header"""
+
+
+import requests
+import sys
+
+url_take = sys.argv
+if len(url_take) < 2:
+        print("Please, Provide url address next to cmd")
+        exit(1)
+
+if __name__ == "__main__":
+    if len(url_take) == 2:
+        response = requests.get(url_take[1])
+
+        if response.status_code == 200:
+            print(response.text)
+        elif response.status_code >= 400:
+             print("Error code:",response.status_code)
