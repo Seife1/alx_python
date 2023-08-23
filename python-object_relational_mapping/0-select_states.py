76c484f8ebc3567e336155e22ea92fc15f74078e
Seife1 <gebrealabate19@yahoo.com>
2023-08-23 09:11:22 +0300

    PEP validation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,10 @@ user = sys.argv
 username = user[1]
 password = user[2]
 db = user[3]
+
+
 def main(username, password, db):
-    
+ 
     mydb = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=db, charset="utf8")
     cursor = mydb.cursor()
