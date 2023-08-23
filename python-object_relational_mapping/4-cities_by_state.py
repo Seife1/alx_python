6ab9fec96af1f02a60ce4b54a4bb6a76c6021fee
Seife1 <gebrealabate19@yahoo.com>
2023-08-23 17:44:14 +0300

    Join

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -11,12 +11,13 @@ def main(username, pw, db):
     mydb = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=pw, db=db, charset="utf8")
     cursor = mydb.cursor()
-    query = """ SELECT cities.id, cities.name, states.name 
-                FROM cities 
-                NATURAL JOIN states
+    query = """ SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON cities.state_id = states.id
                 ORDER BY cities.id ASC
             """
-    
+
     cursor.execute(query)
 
     response = cursor.fetchall()
