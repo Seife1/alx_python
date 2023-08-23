2b97b4716bdfc64b047d7237fc1c9bb9dff1f88f
Seife1 <gebrealabate19@yahoo.com>
2023-08-23 17:18:27 +0300

    Subquery

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+"""Write a script that lists all cities from the database """
+import MySQLdb
+import sys
+
+
+args = sys.argv
+
+
+def main(username, pw, db):
+
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=pw, db=db, charset="utf8")
+    cursor = mydb.cursor()
+    query = """ SELECT cities.id, cities.name, states.name 
+                FROM cities 
+                NATURAL JOIN ON states
+                ORDER BY cities.id ASC
+            """
+    
+    cursor.execute(query)
+
+    response = cursor.fetchall()
+    for row in response:
+        print(row)
+
+if __name__ == "__main__":
+    username = args[1]
+    pw = args[2]
+    db = args[3]
+
+    main(username, pw, db)
