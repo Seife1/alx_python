89f88a1a2dab0255296db5007d482ad02fe13679
Seife1 <gebrealabate19@yahoo.com>
2023-08-23 09:01:35 +0300

    input

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,11 @@ from the database hbtn_0e_0_usa
 """
 import MySQLdb
 
+
 def main(username, password, db):
-    mydb = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db, charset="utf8")
+    
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db, charset="utf8")
     cursor = mydb.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     results = cursor.fetchall()
@@ -12,7 +15,7 @@ def main(username, password, db):
         print(row)
 
 if __name__ == "__main__":
-    username = "alx"
-    password = "ALXmfBright"
-    db = "hbtn_0e_0_usa"
+    username = input("Enter MySQL username: ")
+    password = input("Enter MySQL password: ")
+    db = input("Enter database name: ")
     main(username, password, db)
